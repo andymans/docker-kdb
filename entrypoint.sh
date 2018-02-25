ae32872b758f85fc668bda237d436cf548470cf8
Andy Manson <andy@clippet.co>
2018-02-25 13:18:18 +0000

    added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# A simple entrypoint script - it prints:
+# 1 - prints a welcome message
+# 2 - prints the length of args
+# 3 - executes the arguments passed in. 
+# Naturally it can be customised...to perform checks, start various services, etc.,
+# One good use cae for the entrypoint would be to pass in a specific script and port to run. in this case a 'master' script like master.q could start on e.g. port 5000 and a set of worker scripts worker-1.q, worker-2.q etc., could run on 5001, 5002...
+echo "Welcome to KDB+ on Docker"
+echo $#
+exec "$@"
