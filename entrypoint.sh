805b9362953b493c2cd170b6b02415f3ad855cd2
Andy Manson <andy@clippet.co>
2018-02-26 13:10:55 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # 2 - prints the length of args
 # 3 - executes the arguments passed in. 
 # Naturally it can be customised...to perform checks, start various services, etc.,
-# One good use cae for the entrypoint would be to pass in a specific script and port to run. in this case a 'master' script like master.q could start on e.g. port 5000 and a set of worker scripts worker-1.q, worker-2.q etc., could run on 5001, 5002...
+# One good use case for the entrypoint would be to pass in a specific script and port to run. in this case a 'master' script like master.q could start on e.g. port 5000 and a set of worker scripts worker-1.q, worker-2.q etc., could run on 5001, 5002...
 echo "Welcome to KDB+ on Docker"
 echo $#
 exec "$@"
